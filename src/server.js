9015b59bd0e304b212b0dd7428d3408c36a96c27
sanjukumari-tech <foodlazzez@gmail.com>
2024-08-14 15:32:46 +0530

    this is second commit

--- src/server.js
+++ src/server.js
@@ -1,14 +1,20 @@
 const  express = require("express");
+const connectToDB = require("./config/db");
+require('dotenv').config();
 
 const  app = express();
+const db_url = process.env.DB_URL;
 
 const port = process.env.PORT ||8999;
+
 app.get('/',(req,res)=>{
     res.send("home route")
 })
 
-app.listen(port ,()=>{
+app.listen(port ,async()=>{
     try{
+        await connectToDB(db_url);
+        console.log("we are successfully connected to database")
         console.log(`server is running at http://localhost:${port}`)
     }catch(err){
         console.log(err);
